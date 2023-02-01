4f85c78ad1b9fc5983a76e80af11462ced2580ae
Muhammadrizo14 <mtursunboev13@gmail.com>
2023-02-01 20:20:09 +0500

    added preloader

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ const showPrevSlide = () => {
     imagesSlides('up')
 }
 
-
+window.onload = function() {
+     var preloader = document.getElementById('page-preloader');
+        if( !preloader.classList.contains('done') )
+        {
+            preloader.classList.add('done');
+        }
+};
 
 if (window.innerWidth >= 768) {
     window.addEventListener('wheel', (e) => {
